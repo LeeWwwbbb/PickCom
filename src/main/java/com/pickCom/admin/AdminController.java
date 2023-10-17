246ed18c1d789a9b83a90285b502e574b7703494
pojun406 <pojun406@naver.com>
2023-10-17 22:13:51 +0900

    update AdminService

--- src/main/java/com/pickCom/admin/AdminController.java
+++ src/main/java/com/pickCom/admin/AdminController.java
@@ -1,59 +1,58 @@
 package com.pickCom.admin;
 
-import java.io.IOException;
+import com.pickCom.common.CommandMap;
+import com.pickCom.member.MemberDTO;
+import com.pickCom.utils.BoardPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+@Controller
+public class AdminController{
+    @Autowired
+    private AdminService adminService;
 
-@WebServlet("/admin/userList.do")
-public class AdminController extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-    	req.setCharacterEncoding("UTF-8");
-       AdminDAO dao = new AdminDAO();
+    @RequestMapping("/userList")
+    public String userList(Model model,
+                           @RequestParam(value = "searchField", required = false) String searchField,
+                           @RequestParam(value = "searchWord", required = false) String searchWord,
+                           @RequestParam(value = "pageNum", defaultValue = "1") int pageNum) {
 
-        Map<String, Object> map = new HashMap<String, Object>();
+        AdminDAO dao = new AdminDAO();
+        Map<String, Object> map = new HashMap<>();
 
-        String searchField = req.getParameter("searchField");
-        String searchWord = req.getParameter("searchWord");
         if (searchWord != null) {
             map.put("searchField", searchField);
             map.put("searchWord", searchWord);
         }
+
         int totalCount = dao.selectCount(map);
+        int pageSize = Integer.parseInt("YOUR_PAGE_SIZE"); // 페이지 크기 설정
+        int blockPage = Integer.parseInt("YOUR_BLOCK_SIZE"); // 페이지 블록 크기 설정
 
-        ServletContext application = getServletContext();
-        int pageSize = Integer.parseInt(application.getInitParameter("POSTS_PER_PAGE"));
-        int blockPage = Integer.parseInt(application.getInitParameter("PAGES_PER_BLOCK"));
-
-        int pageNum = 1; 
-        String pageTemp = req.getParameter("pageNum");
-        if (pageTemp != null && !pageTemp.equals(""))
-            pageNum = Integer.parseInt(pageTemp); 
-
-        int start = (pageNum - 1) * pageSize + 1; 
-        int end = pageNum * pageSize; 
+        int start = (pageNum - 1) * pageSize + 1;
+        int end = pageNum * pageSize;
         map.put("start", start);
         map.put("end", end);
 
         List<MemberDTO> userLists = dao.selectListUser(map);
 
-        String pagingImg = BoardPage.pagingStr(totalCount, pageSize,
-                blockPage, pageNum, "../admin/userList.do"); 
+        String pagingImg = BoardPage.pagingStr(totalCount, pageSize, blockPage, pageNum, "/admin/userList");
         map.put("pagingImg", pagingImg);
         map.put("totalCount", totalCount);
         map.put("pageSize", pageSize);
         map.put("pageNum", pageNum);
 
-        req.setAttribute("userLists", userLists);
-        req.setAttribute("map", map);
-        req.getRequestDispatcher("/admin/userManager.jsp").forward(req, resp);
+        model.addAttribute("userLists", userLists);
+        model.addAttribute("map", map);
+
+        return "admin/userManager";
     }
 }
