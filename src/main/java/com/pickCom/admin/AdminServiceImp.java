801983389a408060a7f5a0b580c04910abceeef3
pojun406 <pojun406@naver.com>
2023-10-18 11:03:22 +0900

    .

---
+++ src/main/java/com/pickCom/admin/AdminServiceImp.java
@@ -0,0 +1,22 @@
+package com.pickCom.admin;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Service("adminService")
+public class AdminServiceImp implements AdminService {
+    @Resource(name = "adminDAO")
+    private AdminDAO adminDAO;
+
+    @Override
+    public void MemberDelete(Map<String, Object> map) throws Exception {
+        adminDAO.MemberDelete(map);
+    }
+
+    @Override
+    public void MemberUpdate(Map<String, Object> map) throws Exception {
+        adminDAO.MemberUpdate(map);
+    }
+}
