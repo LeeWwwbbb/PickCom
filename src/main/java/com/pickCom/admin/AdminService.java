fb25a7429dfea238f593eb078a75fc6a8efdcd43
pojun406 <pojun406@naver.com>
2023-10-23 18:49:49 +0900

    admin

--- src/main/java/com/pickCom/admin/AdminService.java
+++ src/main/java/com/pickCom/admin/AdminService.java
@@ -7,10 +7,9 @@ public interface AdminService {
     // 유저 리스트
     List<Map<String, Object>> MemberList(Map<String, Object> map) throws Exception;
     // 유저 강제 탈퇴
-    public void MemberDelete(Map<String, Object> map) throws Exception;
+    public int MemberDelete(String member) throws Exception;
     // 유저 정보 업데이트
     public void MemberUpdate(Map<String, Object> map) throws Exception;
 
-    int MemberCount(Map<String, Object> map) throws Exception;
 }
 
