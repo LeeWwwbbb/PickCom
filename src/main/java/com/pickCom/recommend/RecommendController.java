bcd9eb20d1cde11d14c482dfc1ba6752f5b1977d
LeeWwwbbb <roozi8855@kyungmin.ac.kr>
2023-11-19 16:53:19 +0900

    수정

--- src/main/java/com/pickCom/recommend/RecommendController.java
+++ src/main/java/com/pickCom/recommend/RecommendController.java
@@ -1,12 +1,14 @@
 package com.pickCom.recommend;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 
+@Controller
 public class RecommendController {
-    @Resource(name = "RecommendServiceImp")
+    @Resource(name = "recommendServiceImp")
     private RecommendService recommendService;
 
     @RequestMapping(value = "/recommend")
--- src/main/java/com/pickCom/recommend/RecommendServiceImp.java
+++ src/main/java/com/pickCom/recommend/RecommendServiceImp.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
 @Service("recommendServiceImp")
-public class RecommendServiceImp {
+public class RecommendServiceImp implements RecommendService{
     @Resource(name = "recommendDAO")
     private RecommendDAO recommendDAO;
+
+    @Override
+    public void recommend() throws Exception{
+
+    }
 }
